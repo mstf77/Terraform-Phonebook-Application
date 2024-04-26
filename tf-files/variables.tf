9b3fe823f346e286cc191d7808ab8265270b1e69
mustafa <mstf.alsancak7@gmail.com>
2024-04-26 14:31:53 +0300

    file added

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "james-clarusway"
+}
+
+variable "git-token" {
+  default = "XXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "mykey"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
